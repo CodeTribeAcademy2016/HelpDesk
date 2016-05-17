ab0f7a35791aecda58d3c5d116d3e6a525535b95
Magadani <johannes.magadani@gmail.com>
2016-05-17 11:50:10 -0700

    Five tabs added for admin main page

---
+++ app/src/main/java/za/co/codetribe/userhelpdesk/helpdeskadmin/Admin_UserTab.java
@@ -0,0 +1,31 @@
+package za.co.codetribe.userhelpdesk.helpdeskadmin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.user.userhelpdesk.R;
+
+public class Admin_UserTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__user_tab);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
