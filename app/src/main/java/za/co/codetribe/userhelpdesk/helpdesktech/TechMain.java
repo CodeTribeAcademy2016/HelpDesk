3422e65cefe1837f7a304d8dfba6febf81a96726
Magadani <johannes.magadani@gmail.com>
2016-07-05 08:34:50 -0700

    Technician Changes

--- app/src/main/java/za/co/codetribe/userhelpdesk/helpdesktech/TechMain.java
+++ app/src/main/java/za/co/codetribe/userhelpdesk/helpdesktech/TechMain.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -26,9 +28,15 @@ import java.util.Date;
 import za.co.codetribe.userhelpdesk.Profile;
 import za.co.codetribe.userhelpdesk.R;
 import za.co.codetribe.userhelpdesk.helpdeskadmin.MainAdmin;
+import za.co.codetribe.userhelpdesk.helpdesktech.adapter.ViewPagerAdapter;
 
 public class TechMain extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
 
     String jsonString;
